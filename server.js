fe0070c8927f3a1ddc0505f04035ef9229fc8313
Chineze Nwosu <chineze.nwosu@andela.com>
2017-01-05 10:49:49 +0100

    serve app with node js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express  = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+// listen (start app with node server.js)
+app.listen(port);
+console.log("Angela listening on port", port);
+
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
