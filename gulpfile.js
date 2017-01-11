a3e10e04c08c1d7fc4e9bfa24d9e9b6b649ec2bd
Chineze Nwosu <chineze.nwosu@andela.com>
2017-01-11 17:34:09 +0100

    rearrange product structure, minify css and js in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,56 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var rename = require('gulp-rename');
-var jshint = require('gulp-jshint');
 var plumber = require('gulp-plumber');
-var dotenv = require('dotenv');
-dotenv.load();
+var nodemon = require('gulp-nodemon');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+require('dotenv').load();
+var port = process.env.PORT || 8000;
 
 var isProduction = process.env.NODE_ENV == 'production'
 
-gulp.task('html', function(){
-  return gulp.src('app/*.jade')
-      .pipe(jshint())
+gulp.task('build-html', function(){
+  return gulp.src('app/**/*.jade')
       .pipe(jade({ pretty: !isProduction }))
       .pipe(gulp.dest('public'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/*.jade', ['html']);
+gulp.task('build-css', function(){
+  return gulp.src('app/css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(concat('application.css'))
+      .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('build-js', function(){
+  return gulp.src('app/js/*.js')
+      .pipe(uglify())
+      .pipe(concat('application.js'))
+      .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('serve', function(){
+  nodemon({
+    script: 'server.js',
+    watch: ["server.js", 'public/*', 'public/*/**'],
+    ext: 'js'
+  }).on('restart', function(){
+    console.log("Server restarting...");
+  });
+});
+
+gulp.task('launch', ['serve'], function() {
+  browserSync.init({
+    proxy: "http://localhost:8000"
+  });
+});
+
+gulp.task('watch', ['build-html', 'build-css', 'build-js'], function(){
+  gulp.watch('app/*.jade', ['build-html', 'build-css', 'build-js'])
+      .on('change', browserSync.reload);
+});
+
+gulp.task('default', ['launch', 'watch']);
